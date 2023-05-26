746efd481b746235f87028ff93978651f5d4856b
Itay Hasid <itay080297@gmail.com>
2023-05-26 10:52:28 +0300

    part1

---
+++ checkers/board.py
@@ -0,0 +1,16 @@
+import pygame
+from .constants import BLACK, ROWS, RED, SQUARE_SIZE, COLS, WHITE
+
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.red_left = self.white_left = 12
+        self.red_kings = self.white_kings = 0
+
+    def draw_squares(self, win):
+        win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row % 2, COLS, 2):
+                pygame.draw.rect(win, RED, (row*SQUARE_SIZE,
+                                 col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
